bc67cdb1d008a0081d8bd01fc6dd5eafbb56d19c
Henrique Bruno <henrique.bruno.fa@gmail.com>
2021-08-09 16:24:33 -0300

    Added tsconfig.lint.json for linting

--- semitemplates/expo-pkg/.eslintrc.js
+++ semitemplates/expo-pkg/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
     "tsconfigRootDir": __dirname, // *1
-    "project": ['./tsconfig.json'], // *1
+    "project": ['./tsconfig.lint.json'], // *1
     "ecmaVersion": 12,
     "sourceType": "module",
     "ecmaFeatures": { // *2
