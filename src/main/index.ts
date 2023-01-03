0ff7094093c3d2dfd1c8807760ef1b7beded5c7f
Henrique Almeida <henrique.bruno.fa@gmail.com>
2023-01-03 00:17:39 +0000

    move to swc

--- src/main/index.ts
+++ src/main/index.ts
@@ -34,63 +34,101 @@ import type { PackageManager } from './types.js';
 
 const program = new Command();
 
-
 // TODO add -v as alias to -V/--version.
 program
   .name('gev') // So it appears in the usage help
   .version(Program.version, '-v, --version', 'Output the version number.') // Just capitalize the first letter of description.
   .helpOption('-h, --help', 'Display help for command.') // Just capitalize the first letter of description.
-  .option('-n, --no-install', 'Don\'t install the npm packages after setting the template.')
-  .option('-c, --no-check-latest', 'Won\'t check if is using the latest version of gev.')
+  .option(
+    '-n, --no-install',
+    "Don't install the npm packages after setting the template.",
+  )
+  .option(
+    '-c, --no-check-latest',
+    "Won't check if is using the latest version of gev.",
+  )
   .option('--npm', 'Use npm as package manager')
   .option('--pnpm', 'Use pnpm as package manager (default)')
   .option('--yarn', 'Use yarn as package manager')
-  .option('-C, --no-clean-on-error', 'Won\'t clean the project being generated if an error happened.')
+  .option(
+    '-C, --no-clean-on-error',
+    "Won't clean the project being generated if an error happened.",
+  )
   // https://github.com/tj/commander.js/issues/518#issuecomment-872769249
-  .addArgument(new Argument('<flavor>', `The project kind.`)
-    .choices(availableFlavors)) // Will also print in the usage the possible options
-  .argument('[projectName]', 'The name of the new project. A new directory will be created and used only if it doesn\'t exists. If ommited or \'.\', will use the current directory and its name, if empty.')
-  .description('Effortlessly creates slightly opionated projects boilerplates within a single command.')
+  .addArgument(
+    new Argument('<flavor>', `The project kind.`).choices(availableFlavors),
+  ) // Will also print in the usage the possible options
+  .argument(
+    '[projectName]',
+    "The name of the new project. A new directory will be created and used only if it doesn't exists. If ommited or '.', will use the current directory and its name, if empty.",
+  )
+  .description(
+    'Effortlessly creates slightly opionated projects boilerplates within a single command.',
+  )
   .showHelpAfterError()
   .action(async () => {
     // Commander transforms --no-install into install, with default value of true to install.
     // https://github.com/tj/commander.js#other-option-types-negatable-boolean-and-booleanvalue
     const {
       install: installPackages,
-      checkLatest, cleanOnError,
-      npm, yarn, pnpm,
+      checkLatest,
+      cleanOnError,
+      npm,
+      yarn,
+      pnpm,
     } = program.opts<{
-      install: boolean; checkLatest: boolean; cleanOnError: boolean;
-      npm: boolean; yarn: boolean; pnpm: boolean;
+      install: boolean;
+      checkLatest: boolean;
+      cleanOnError: boolean;
+      npm: boolean;
+      yarn: boolean;
+      pnpm: boolean;
     }>();
-    const [flavor, projectRelativePath = '.'] = program.args as [string, string | undefined];
+    const [flavor, projectRelativePath = '.'] = program.args as [
+      string,
+      string | undefined,
+    ];
 
     const defaultPackageManager: PackageManager = 'pnpm';
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    const packageManager: PackageManager = (npm && 'npm') || (yarn && 'yarn') || (pnpm && 'pnpm') || defaultPackageManager;
+    const packageManager: PackageManager =
+      (npm && 'npm') ||
+      (yarn && 'yarn') ||
+      (pnpm && 'pnpm') ||
+      defaultPackageManager;
 
     if (checkLatest) {
       // Ensure latest version
       const spinner = ora().start('Ensuring latest version');
       const latestVer = await latestVersion('gev');
       if (compareSemver(Program.version, latestVer) === -1) {
-        spinner.info(`The current version of gev [${chalk.grey(Program.version)}] is lower than the latest one [${chalk.yellow(latestVer)}]. Recalling gev with @latest.\n`); // Additional \n
+        spinner.info(
+          `The current version of gev [${chalk.grey(
+            Program.version,
+          )}] is lower than the latest one [${chalk.yellow(
+            latestVer,
+          )}]. Recalling gev with @latest.\n`,
+        ); // Additional \n
 
         const rawProgramArgs = process.argv.slice(2);
-        await execa('npx', ['gev@latest', '--no-check-latest', ...rawProgramArgs], {
-          stdio: 'inherit',
-          env: {
-            npm_config_yes: 'true', // https://github.com/npm/cli/issues/2226#issuecomment-732475247
+        await execa(
+          'npx',
+          ['gev@latest', '--no-check-latest', ...rawProgramArgs],
+          {
+            stdio: 'inherit',
+            env: {
+              npm_config_yes: 'true', // https://github.com/npm/cli/issues/2226#issuecomment-732475247
+            },
           },
-        }).catch(null); // ignore throw here. It will already be treated in the @latest.
+        ).catch(null); // ignore throw here. It will already be treated in the @latest.
         return;
-      } else { // Same version. We are running the latest one!
+      } else {
+        // Same version. We are running the latest one!
         spinner.succeed();
       }
     }
 
     await loadConfigs();
-
 
     let githubAuthor = configData.githubAuthor;
 
@@ -98,14 +136,18 @@ program
 
     if (canReadStdin) {
       const input = await inquirer.prompt<{ githubAuthor: string }>([
-        { name: 'githubAuthor', type: 'input', default: githubAuthor, message: 'Who is the GitHub Author of this project?' },
+        {
+          name: 'githubAuthor',
+          type: 'input',
+          default: githubAuthor,
+          message: 'Who is the GitHub Author of this project?',
+        },
       ]);
       githubAuthor = input.githubAuthor;
 
       if (githubAuthor !== configData.githubAuthor)
         await setConfigs({ githubAuthor });
     }
-
 
     const core = new Core({
       flavor,
@@ -117,17 +159,18 @@ program
     });
 
     await core.run();
-  })
-;
-
+  });
 
 program.parseAsync().catch((err) => {
   let msg;
-  if (typeof err === 'object' && err !== null)
-    msg = err.message;
-  else
-    msg = err;
-  console.error(`\n${chalk.redBright('An error happened!')} ${chalk.white('-')} ${chalk.yellow(`[gev v${Program.version}]`)}\n`);
+  if (typeof err === 'object' && err !== null) msg = err.message;
+  else msg = err;
+  console.error(
+    `\n${chalk.redBright('An error happened!')} ${chalk.white(
+      '-',
+    )} ${chalk.yellow(`[gev v${Program.version}]`)}\n`,
+  );
   console.error(msg);
+  console.error(err.stack);
   process.exit(1); // Good for external tools.
 });
