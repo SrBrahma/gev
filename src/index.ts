4c7b4d4c36c6faf0100203cc142c68db1ba5565b
Henrique Almeida <henrique.bruno.fa@gmail.com>
2023-12-17 22:38:18 +0100

    3.0.13

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env bun
 import path from 'path';
 import chalk from 'chalk';
 import { Argument, Command } from 'commander';
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,12 @@
 import { readdirSync, readFileSync } from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { execaCommand, execaSync } from 'execa';
 
-export const pathFromRoot = (...p: Array<string>) => path.resolve(import.meta.dir, '..', ...p);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const pathFromRoot = (...p: Array<string>) => path.resolve(__dirname, '..', ...p);
 export const { version } = JSON.parse(readFileSync(pathFromRoot('./package.json'), 'utf8')) as {
   version: string;
 };
